b2bd30d4fb80e294c91b6f66167c644eb67d2f94
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-03-31 08:37:13 +0200

    clean sorties

--- sorties/__main__.py
+++ sorties/__main__.py
@@ -1,5 +1,4 @@
-from .settings import hosts
 from .sortie_ap import SortieAGVPrint as default
 
 if __name__ == '__main__':
-    default(host=hosts.yuki).loop()
+    default().loop()
--- sorties/sortie_ap.py
+++ sorties/sortie_ap.py
@@ -13,39 +13,37 @@ class SortieAGVPrint(Sortie):
         self.connect()
 
     def connect(self):
-        try_again = True
-        while try_again:
+        while True:
             try:
-                try_again = False
                 self.socket.close()
                 self.socket = socket()
                 self.socket.settimeout(2)
                 print('connecting... %s:%i' % (AGV_HOST[self.host], AGV_PORT))
                 self.socket.connect((AGV_HOST[self.host], AGV_PORT))
                 print('connected')
+                break
             except timeout:
-                try_again = True
+                print('timeout…')
             except ConnectionRefusedError:
-                sleep(5)
+                print('Connection Refused……')
 
     def process(self):
         pprint(self.state)
-        ret = ''
         try:
             self.socket.sendall(self.send_agv())
             ret = self.socket.recv(1024).decode('ascii')
-        except ConnectionResetError:
+            if ret.startswith('+'):  # Les erreurs commencent par un +
+                print(ret)
+                if ret[1] != '4':
+                    raise RuntimeError(ret)
+        except (ConnectionResetError, timeout):
             self.connect()
-        except timeout:
-            print('\t\t\ttimeout…')
-            self.connect()
-        if ret.startswith('+'):  # Les erreurs commencent par un +
-            print(ret)
-            if ret[1] != '4':
-                raise RuntimeError(ret)
 
     def send_agv(self):
         if self.state['stop']:
             return b'stop()'
         template = 'setSpeedAndPosition({v1}, {t1}, {v2}, {t2}, {v3}, {t3})'
         return bytes(template.format(**self.state).encode('ascii'))
+
+if __name__ == '__main__':
+    SortieAGVPrint().loop()
