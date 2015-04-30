45c6b5d0bb2f85587e868aa5c9bd0a0fc3e384e7
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-04-30 13:11:29 +0200

    print

---
+++ sorties/print.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+from time import sleep
+
+from ..settings import PERIODE
+from ..vmq import Subscriber, vmq_parser
+
+
+class SortiePrint(Subscriber):
+    def loop(self):
+        pprint(self.data)
+        sleep(PERIODE)
+
+
+if __name__ == '__main__':
+    SortiePrint(**vars(vmq_parser.parse_args())).run()
