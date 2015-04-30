6d97a786fb65c596a579cc4678ba2e7849bd52b1
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-04-30 19:33:15 +0200

    fu

--- home_scripts/sync.sh
+++ home_scripts/sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-pssh -H cerf -H moro -H ame -H yuki 'cd venise; git pull'
+pssh -H cerf -H moro -H ame -H yuki 'cd ~/venise; git pull'
+pssh -H moro -H ame -H yuki 'find ~/venise/ -name '*.pyc' -delete; find ~/venise/ -name __pycache__ -delete'
 pssh -H moro -H ame -H yuki 'sudo systemctl restart agv'
