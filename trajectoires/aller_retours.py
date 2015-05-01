d5ffdd8406f380ea48a76ca4d11e97b11bf8bb80
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-01 02:49:53 +0200

    le fichier que j’ai oublié d’envoyer y’a une éternité

---
+++ trajectoires/aller_retours.py
@@ -0,0 +1,10 @@
+from .points import trajectoire_points_parser, TrajectoirePoints
+from ..settings import ALLER_RETOURS
+
+
+class TrajectoireAllerRetours(TrajectoirePoints):
+    def get_paths(self):
+        return ALLER_RETOURS
+
+if __name__ == '__main__':
+    TrajectoireAllerRetours(**vars(trajectoire_points_parser.parse_args())).run()
