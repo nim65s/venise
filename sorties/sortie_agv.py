9b490e37780a8b246588a7cbd94ad9ab049799ff
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-01 03:11:00 +0200

    oops

--- sorties/sortie_agv.py
+++ sorties/sortie_agv.py
@@ -127,8 +127,8 @@ class SortieAGV(Sortie):
         if (abs(array(self.data[self.hote]['tt']) - array(self.data[self.hote]['tm'])) < 0.01).all():
             self.push.send_json([self.hote, {'force': False}])
             return
-        self.data['vc'] = [5, 5, 5]
-        self.data['tc'] = self.data['tt']
+        self.data[self.hote]['vc'] = [5, 5, 5]
+        self.data[self.hote]['tc'] = self.data['tt']
 
 
 if __name__ == '__main__':
