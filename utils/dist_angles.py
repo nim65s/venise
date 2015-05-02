1ec5b63f3455fb8b1054f2bba0bc8cdd0656b33d
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-02 15:17:54 +0200

    dist_angles en array

--- utils/dist_angles.py
+++ utils/dist_angles.py
@@ -1,9 +1,11 @@
 from math import pi
+from numpy import array
+
 def dist_angles(actuel, cible):
-    dst = actuel - cible
-    while dst < -pi:
-        dst += 2 * pi
-    while dst > pi:
-        dst -= 2 * pi
+    dst = array(actuel) - array(cible)
+    while (dst < -pi).any():
+        dst[where(dst < -pi)] += 2 * pi
+    while (dst > pi).any():
+        dst[where(dst > pi)] -= 2 * pi
     return dst
 
