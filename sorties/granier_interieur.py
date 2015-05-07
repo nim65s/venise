41baa69d99e69502f8acc3f2ef0fea084dc2fe72
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-05-07 13:19:03 +0200

    minitre

--- sorties/granier_interieur.py
+++ sorties/granier_interieur.py
@@ -1,6 +1,8 @@
 from numpy import array
+from time import sleep
+
 from ..vmq import vmq_parser, Subscriber, Pusher
-from ..settings import Hote
+from ..settings import Hote, N_SONDES
 
 
 class SortieGranierInterieur(Subscriber, Pusher):
@@ -10,8 +12,19 @@ class SortieGranierInterieur(Subscriber, Pusher):
         self.hotes = [Hote.ame, Hote.yuki]
 
     def loop(self):
-        self.data[self.hote]['granier'] = array([self.data[h]['granier'] for h in self.hotes]).mean(axis=0).round(3).tolist()
-        self.push.send_json([self.hote, {'granier': self.data[self.hote]['granier']}])
+        self.sub()
+        self.data[self.hote].update(**self.process_granier_interieur(**self.data[self.hote]))
+        sleep(15)
+
+    def process_granier_interieur(self, granier, gmi, gma, gm, **kwargs):
+        granier = array([self.data[h]['granier'] for h in self.hotes]).mean(axis=0).round(3).tolist()
+        for i in range(N_SONDES):
+            gmi[i] = min(granier[i], gmi[i])
+            gma[i] = max(granier[i], gma[i])
+            gm[i] = round((granier[i] - gmi[i]) / (gma[i] - gmi[i] if gma[i] != gmi[i] else 1), 5)
+        d = {'granier': granier, 'gma': gma, 'gmi': gmi, 'gm': gm}
+        self.push.send_json([self.hote, d])
+        return d
 
 
 if __name__ == '__main__':
