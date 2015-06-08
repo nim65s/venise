212674efbb98d12869a3ca8e603ecb2226b7b8f1
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-08 19:39:12 +0200

    encore une journée de boulot

---
+++ sorties/is_up.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+from ..vmq import Pusher, Subscriber, vmq_parser
+
+
+class IsUp(Subscriber, Pusher):
+    def loop(self):
+        self.sub(block=0)
+        for h in self.hotes:
+            is_up = self.check_up(**self.data[h])
+            self.push.send_json([h, {'is_up': is_up}])
+            if not is_up:
+                self.push.send_json([h, {'status': 'AGV éteint'}])
+
+    def check_up(self, hote, last_seen_agv, **kwargs):
+        try:
+            return (datetime.now() - datetime.strptime(last_seen_agv, '%Y-%m-%d %H:%M:%S.%f')) < timedelta(seconds=2)
+        except:
+            return False
+
+
+if __name__ == '__main__':
+    IsUp(**vars(vmq_parser.parse_args())).run()
