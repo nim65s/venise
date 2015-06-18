8fb387b70f898dd4600872e8356a91029f53b50c
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-18 09:43:35 +0200

    trajectoires/__main__.py

--- trajectoires/__main__.py
+++ trajectoires/__main__.py
@@ -1,5 +1,10 @@
-from .manuel import TrajectoireManuelle as Trajectoire
-from .trajectoire import trajectoire_parser as parser
-
 if __name__ == '__main__':
+    from .points import trajectoire_points_parser as parser
+    from ..settings import PROD
+
+    if PROD:
+        from .demo import TrajectoireDemo as Trajectoire
+    else:
+        from .granier import TrajectoireGranier as Trajectoire
+
     Trajectoire(**vars(parser.parse_args())).run()
