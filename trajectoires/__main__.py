5d900ed8ee47e4e842231b7b2c963807b42ef3d3
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-06-25 10:02:55 +0200

    debug

--- trajectoires/__main__.py
+++ trajectoires/__main__.py
@@ -3,8 +3,8 @@ if __name__ == '__main__':
     from ..settings import PROD
 
     if PROD:
-        from .demo import TrajectoireDemo as Trajectoire
-    else:
         from .granier import TrajectoireGranier as Trajectoire
+    else:
+        from .demo import TrajectoireDemo as Trajectoire
 
     Trajectoire(**vars(parser.parse_args())).run()
