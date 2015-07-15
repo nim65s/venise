077e17fe62d7c40c814af042ed620b87c2ab999f
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-07-15 13:38:59 +0200

    pep8

--- utils/point_in_polygon.py
+++ utils/point_in_polygon.py
@@ -11,9 +11,11 @@
 
 # translated to Python by Maciej Kalisiak <mac@dgp.toronto.edu>
 
+# pep8 by Guilhem Saurel <gsaurel@laas.fr>
+
 #   a Point is represented as a tuple: (x,y)
 
-#===================================================================
+# ==================================================================
 
 # is_left(): tests if a point is Left|On|Right of an infinite line.
 
@@ -23,55 +25,58 @@
 #           <0 for P2 right of the line
 #   See: the January 2001 Algorithm "Area of 2D and 3D Triangles and Polygons"
 
-def is_left(P0, P1, P2):
-    return (P1[0] - P0[0]) * (P2[1] - P0[1]) - (P2[0] - P0[0]) * (P1[1] - P0[1])
 
-#===================================================================
+def is_left(p0, p1, p2):
+    return (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1])
 
-# cn_PnPoly(): crossing number test for a point in a polygon
+# ==================================================================
+
+# cn_pn_poly(): crossing number test for a point in a polygon
 #     Input:  P = a point,
 #             V[] = vertex points of a polygon
 #     Return: 0 = outside, 1 = inside
 # This code is patterned after [Franklin, 2000]
 
-def cn_PnPoly(P, V):
+
+def cn_pn_poly(p, v):
     cn = 0    # the crossing number counter
 
     # repeat the first vertex at end
-    V = tuple(V[:])+(V[0],)
+    v = tuple(v[:]) + (v[0],)
 
     # loop through all edges of the polygon
-    for i in range(len(V)-1):   # edge from V[i] to V[i+1]
-        if ((V[i][1] <= P[1] and V[i+1][1] > P[1])   # an upward crossing
-            or (V[i][1] > P[1] and V[i+1][1] <= P[1])):  # a downward crossing
+    for i in range(len(v) - 1):   # edge from V[i] to V[i+1]
+        if ((v[i][1] <= p[1] and v[i + 1][1] > p[1])   # an upward crossing
+                or (v[i][1] > p[1] and v[i + 1][1] <= p[1])):  # a downward crossing
             # compute the actual edge-ray intersect x-coordinate
-            vt = (P[1] - V[i][1]) / float(V[i+1][1] - V[i][1])
-            if P[0] < V[i][0] + vt * (V[i+1][0] - V[i][0]): # P[0] < intersect
+            vt = (p[1] - v[i][1]) / float(v[i + 1][1] - v[i][1])
+            if p[0] < v[i][0] + vt * (v[i + 1][0] - v[i][0]):  # P[0] < intersect
                 cn += 1  # a valid crossing of y=P[1] right of P[0]
 
     return cn % 2   # 0 if even (out), and 1 if odd (in)
 
-#===================================================================
+# ==================================================================
 
-# wn_PnPoly(): winding number test for a point in a polygon
+# wn_pn_poly(): winding number test for a point in a polygon
 #     Input:  P = a point,
 #             V[] = vertex points of a polygon
 #     Return: wn = the winding number (=0 only if P is outside V[])
 
-def wn_PnPoly(P, V):
+
+def wn_pn_poly(p, v):
     wn = 0   # the winding number counter
 
     # repeat the first vertex at end
-    V = tuple(V[:]) + (V[0],)
+    v = tuple(v[:]) + (v[0],)
 
     # loop through all edges of the polygon
-    for i in range(len(V)-1):     # edge from V[i] to V[i+1]
-        if V[i][1] <= P[1]:        # start y <= P[1]
-            if V[i+1][1] > P[1]:     # an upward crossing
-                if is_left(V[i], V[i+1], P) > 0: # P left of edge
+    for i in range(len(v) - 1):     # edge from V[i] to V[i+1]
+        if v[i][1] <= p[1]:        # start y <= P[1]
+            if v[i + 1][1] > p[1]:     # an upward crossing
+                if is_left(v[i], v[i + 1], p) > 0:  # P left of edge
                     wn += 1           # have a valid up intersect
         else:                      # start y > P[1] (no test needed)
-            if V[i+1][1] <= P[1]:    # a downward crossing
-                if is_left(V[i], V[i+1], P) < 0: # P right of edge
+            if v[i + 1][1] <= p[1]:    # a downward crossing
+                if is_left(v[i], v[i + 1], p) < 0:  # P right of edge
                     wn -= 1           # have a valid down intersect
     return wn
