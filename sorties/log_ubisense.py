3805690a83d8b3b0edcc46620a3dec60f6f1524b
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-07-21 10:36:03 +0200

    log positions, warning si trop de différence

---
+++ sorties/log_ubisense.py
@@ -0,0 +1,20 @@
+from math import hypot
+
+from .log import SortieLog, logger_parser
+
+
+class LogUbisense(SortieLog):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pos = 0, 0, 0
+
+    def log(self, logger, x, y, a, **kwargs):
+        if self.pos != x, y, a:
+            log = logger.warning if hypot(x - self.pos[0], y - self.pos[1]) > 0.1 else logger.info
+            log('%f\t%f\t%f' % (x, y, a))
+            self.pos = x, y, a
+
+
+if __name__ == '__main__':
+    logger_parser.set_defaults(logger='ubisense', period=0)
+    LogUbisense(**vars(logger_parser.parse_args())).run()
