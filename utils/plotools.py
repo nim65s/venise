a24c62e9943569bf7f49ed836bdf032a8924f295
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-08-17 16:47:19 +0200

    _from_my_log, plotools

---
+++ utils/plotools.py
@@ -0,0 +1,11 @@
+from pylab import *
+
+
+def plot_bord(bord):
+    data = [[bord[i], bord[(i + 1) % len(bord)]] for i in range(len(bord))]
+    path = []
+    for d in data:
+        path.append((d[0][0], d[1][0]))
+        path.append((d[0][1], d[1][1]))
+        path.append('b')
+    return path
