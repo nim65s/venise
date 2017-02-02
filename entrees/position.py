c2f3fa96d48b99ee35f9935e2a6da7ca662c3741
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-02-02 11:32:49 +0100

    rennes

--- entrees/position.py
+++ entrees/position.py
@@ -15,7 +15,7 @@ class EntreePosition(Entree):
             } for h in Hote}
         self.socket = socket()
         self.connect()
-        self.correction = [0, 0, 2.52, 0.781, 1.3]
+        self.correction = [0, 0, 0, 0, 0]
 
     def connect(self):
         self.socket.close()
