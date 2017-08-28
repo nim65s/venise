90d4517ac946929adc1d97269ee72dfd269b2279
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-08-28 17:22:16 +0200

    clean

--- transhumus/utils/plotools.py
+++
@@ -1,8 +0,0 @@
-def plot_boundary(boundary):
-    data = [[boundary[i], boundary[(i + 1) % len(boundary)]] for i in range(len(boundary))]
-    path = []
-    for d in data:
-        path.append((d[0][0], d[1][0]))
-        path.append((d[0][1], d[1][1]))
-        path.append('b')
-    return path
